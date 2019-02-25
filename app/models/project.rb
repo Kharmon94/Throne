d5af3d80a38dce035e066b28454bc7135fa76f45
smplyamzng <kharmon9452@gmail.com>
2019-02-25 15:47:40 -0500

    Update

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
 
-  has_one_attached :picture, dependent: :destroy
-  has_many_attached :images, dependent: :destroy
+  # has_one_attached :picture, dependent: :destroy
+  # has_many_attached :images, dependent: :destroy
   
 end
