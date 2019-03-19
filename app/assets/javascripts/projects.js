cda60fbcc55bc73a855ab279e909455059dbc2d6
smplyamzng <kharmon9452@gmail.com>
2019-03-19 10:30:59 -0400

    client script fixed

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,5 +1,8 @@
-  document.addEventListener('DOMContentLoaded', function() {
+  
+$(document).on('turbolinks:load', function(){
+  $(document).ready(function() {
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems, options);
     var options = {}
   });
+ })
